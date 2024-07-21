d553f62e673c56cd484cd996814a7181ce828a32
ema <mccoy39082@163.com>
2024-07-21 15:40:26 +0800

    update: add Avalonia demo

---
+++ NotifyIcon.Demo.Avalonia/App.axaml.cs
@@ -0,0 +1,89 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using NotifyIcon = NotifyIconEx.NotifyIcon;
+using ToolStripMenuItem = System.Windows.Forms.ToolStripMenuItem;
+
+namespace AvaloniaApplication1;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+        var notifyIcon = new NotifyIcon()
+        {
+            Text = "NotifyIcon",
+            Icon = Icon.ExtractAssociatedIcon(Process.GetCurrentProcess().MainModule?.FileName!)!,
+        };
+        notifyIcon.AddMenu("MenuItem1", new Bitmap(ResourceHelper.GetStream("avares://NotifyIcon.Demo.Avalonia/Assets/Images/Lock.png")));
+        var toDisableItem = notifyIcon.AddMenu("MenuItem2", new Bitmap(ResourceHelper.GetStream("avares://NotifyIcon.Demo.Avalonia/Assets/Images/Lock.png")));
+        notifyIcon.AddMenu("-");
+        notifyIcon.AddMenu("MenuItem3");
+        notifyIcon.AddMenu("MenuItem4", true);
+        notifyIcon.AddMenu("MenuItem5", OnClick);
+        notifyIcon.AddMenu("-");
+        notifyIcon.AddMenu("SubMenu", null!,
+        [
+            new ToolStripMenuItem("SubMenuItem1"),
+            new ToolStripMenuItem("SubMenuItem2"),
+            new ToolStripMenuItem("SubMenuItem3"),
+            new ToolStripMenuItem("SubSubMenu", null!,
+            [
+                new ToolStripMenuItem("SubSubMenuItem1"),
+                new ToolStripMenuItem("SubSubMenuItem2"),
+                new ToolStripMenuItem("SubSubMenuItem3")
+            ])
+        ]);
+        notifyIcon.AddMenu("-");
+        notifyIcon.AddMenu("Exit", (_, _) => Environment.Exit(0));
+        notifyIcon.BalloonTipShown += OnBalloonTipShown;
+
+        toDisableItem.Enabled = false;
+
+        void OnBalloonTipShown(object? sender, EventArgs e)
+        {
+            Debug.WriteLine("OnBalloonTipShown");
+        }
+
+        void OnClick(object? sender, EventArgs e)
+        {
+            notifyIcon.BalloonTipTitle = "Title";
+            notifyIcon.BalloonTipText = "This Balloon Tips";
+            notifyIcon.ShowBalloonTip(5);
+        }
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        // Line below is needed to remove Avalonia data validation.
+        // Without this line you will get duplicate validations from both Avalonia and CT
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                ///
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
+
+file static class ResourceHelper
+{
+    public static Stream GetStream(string uriString)
+    {
+        Stream stream = AssetLoader.Open(new Uri(uriString));
+        return stream;
+    }
+}
---
+++ NotifyIcon.Demo.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
